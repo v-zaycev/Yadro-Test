c0207681be9ee2229c9efc1bb29a33ead2488f52
unknown <vozon.o@yandex.ru>
2026-05-17 01:34:04 +0300

    Source files added

---
+++ src/scheduler.py
@@ -0,0 +1,119 @@
+# solver.py
+from typing import List, Tuple
+from structs import Consumer, Generator
+
+
+def min_cost_coverage(
+    total_demand: int,
+    generators: List[Generator],
+    hour: int
+) -> Tuple[List[int], int]:
+    """
+    ДП: минимальная стоимость покрытия total_demand.
+    
+    Args:
+        total_demand: суммарный спрос (целое число)
+        generators: все генераторы
+        hour: час (0-23)
+    
+    Returns:
+        (min_cost, selected_indices)
+    """
+    max_power = 0
+    items = []
+    for i, g in enumerate(generators):
+        power = g.hourly_output[hour]
+        max_power += power
+        cost = g.hourly_output[hour] *  g.cost_per_kwh
+        items.append((i, power, cost))
+    
+    if not items:
+        return 0, []
+    
+    INF = 10**18
+    costs = [0] + [INF] * (max_power)
+    last_added_gen = [-1] * (max_power + 1)
+    
+    for i, power, cost in items:
+        if power == 0:
+            continue
+        for m in range(max_power, power - 1, -1):
+            if costs[m - power] != INF and costs[m - power] + cost < costs[m]:
+                costs[m] = costs[m - power] + cost
+                last_added_gen[m] = i
+
+    
+    # Ищем лучший вариант
+    best_cost = INF
+    best_seq_end = INF
+    for m in range(total_demand, max_power + 1):
+        if costs[m] < best_cost:
+            best_cost = costs[m]
+            best_seq_end = m
+    
+    best_indices = []
+    while best_seq_end != 0:
+        index = last_added_gen[best_seq_end]
+        best_indices.append(index)
+        best_seq_end = best_seq_end - items[index][1] # power
+
+    return best_indices, best_cost
+
+
+def max_consumers_coverage(
+    consumers: List[Consumer],
+    generators: List[Generator],
+    hour: int
+) -> Tuple[List[int], List[int], int]:
+    """
+    Перебор генераторов + жадный выбор потребителей (оптимально для максимизации количества).
+    
+    Returns:
+        (selected_consumer_indices, selected_generator_indices, total_cost)
+    """
+    max_power = sum([generator.hourly_output[hour] for generator in generators])
+
+    demands = [(int(c.hourly_demand[hour]), i) for i, c in enumerate(consumers)]
+    demands.sort()
+
+    total_demand = 0
+    total_consumers = 0
+    while total_demand + demands[total_consumers][0] < max_power:
+        total_demand += demands[total_consumers][0]
+        total_consumers += 1
+
+    best_cons = [demands[i][1] for i in range(0, total_consumers)]
+    
+    best_gens, best_cost  = min_cost_coverage(total_demand, generators, hour)
+
+    return best_cons, best_gens, best_cost
+
+
+def schedule_hour(
+    consumers: List[Consumer],
+    generators: List[Generator],
+    hour: int
+) -> dict:
+    """Решает задачу для одного часа"""
+    total_demand = sum(c.hourly_demand[hour] for c in consumers)
+    total_generation = sum(g.hourly_output[hour] for g in generators)
+    
+    if total_generation >= total_demand:
+        # Режим избытка: всех обеспечиваем
+        selected_gens, cost = min_cost_coverage(total_demand, generators, hour)
+        return {
+            'selected_generators': selected_gens,
+            'off_consumers': [],
+            'cost': cost
+        }
+    else:
+        # Режим дефицита: максимизируем количество потребителей
+        selected_cons, selected_gens, cost = max_consumers_coverage(consumers, generators, hour)
+        all_cons = set(range(len(consumers)))
+        off_consumers = list(all_cons - set(selected_cons))
+        
+        return {
+            'selected_generators': selected_gens,
+            'off_consumers': off_consumers,
+            'cost': cost
+        }
---
+++ src/structs.py
@@ -0,0 +1,78 @@
+# models.py
+import json
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Tuple
+
+
+@dataclass
+class Consumer:
+    name: str
+    hourly_demand: List[int]  # 24 значения - потребление в каждый час
+
+
+@dataclass
+class Generator:
+    name: str
+    type: str  # "solar" или "diesel"
+    hourly_output: List[int]  # 24 значения - генерация в каждый час
+    cost_per_kwh: int  # стоимость за кВт·ч
+
+@dataclass
+class TestCase:
+    source_file: str
+    name: str
+    description: str
+    consumers: List[Consumer]
+    generators: List[Generator]
+    
+    def total_consumers(self) -> int:
+        return len(self.consumers)
+    
+    def total_generators(self) -> int:
+        return len(self.generators)
+
+def load_test_case(filepath: str) -> TestCase:
+    """Загружает тестовый случай из JSON"""
+    with open(filepath, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    
+    name = data.get('name', 'Без названия')
+    description = data.get('description', '')
+    
+    consumers = []
+    for c in data.get('consumers', []):
+        consumers.append(Consumer(
+            name=c['name'],
+            hourly_demand=c['hourly_demand']
+        ))
+    
+    generators = []
+    for g in data.get('generators', []):
+        if g['type'] == 'fixed':
+            hourly = [g['constant_output']] * 24
+        else:
+            hourly = g['hourly_output']
+        
+        generators.append(Generator(
+            name=g['name'],
+            type=g['type'],
+            hourly_output=hourly,
+            cost_per_kwh=g['cost_per_kwh']
+        ))
+    
+    return TestCase(
+        source_file=Path(filepath).name, 
+        name=name,
+        description=description,
+        consumers=consumers,
+        generators=generators
+    )
+
+
+def validate_consumers_demands(consumers: List[Consumer]) -> bool:
+    """Проверяет, что у всех потребителей неотрицательный спрос"""
+    for c in consumers:
+        if any(d < 0 for d in c.hourly_demand):
+            return False
+    return True
